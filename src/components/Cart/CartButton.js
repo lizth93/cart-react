2076a14d6965b3dd3cb31ccdd6add31478f46fd2
lizth93 <lizth93@hotmail.com>
2022-06-27 09:59:14 -0500

    Added Cart Slice functionality

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,10 +1,11 @@
 import classes from "./CartButton.module.css";
 
 import { uiActions } from "../store/ui-slice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const CartButton = (props) => {
   const dispatch = useDispatch();
+  const cartQuantity = useSelector((state) => state.cart.totalQuantity);
 
   const showCartHandleButton = () => {
     dispatch(uiActions.toggle());
@@ -13,7 +14,7 @@ const CartButton = (props) => {
   return (
     <button className={classes.button} onClick={showCartHandleButton}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
